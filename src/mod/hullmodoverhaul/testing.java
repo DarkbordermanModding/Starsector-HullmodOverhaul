159cb3cf092cf24bfea606b191decbdcc59cb300
Darkborderman <reastw1234@gmail.com>
2022-08-23 05:43:08 +0800

    refactor: Update

--- src/mod/hullmodoverhaul/testing.java
+++ src/mod/hullmodoverhaul/testing.java
@@ -2,7 +2,6 @@ package mod.hullmodoverhaul;
 
 import com.fs.starfarer.api.combat.BaseHullMod;
 import com.fs.starfarer.api.combat.MutableShipStatsAPI;
-import com.fs.starfarer.api.combat.ShipHullSpecAPI;
 import com.fs.starfarer.api.combat.ShipAPI.HullSize;
 
 public class testing extends BaseHullMod {
@@ -11,8 +10,5 @@ public class testing extends BaseHullMod {
 	}
 
 	public void applyEffectsAfterShipCreation(com.fs.starfarer.api.combat.ShipAPI ship, java.lang.String id) {
-		ShipHullSpecAPI a = new utilities().createNewShipHullSpecAPI(ship.getHullSpec());
-		a = ShipHullSpecAPI.class.cast(a);
-		ship.getVariant().setHullSpecAPI(a);
 	}
 }
